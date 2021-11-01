eb0f7484ad318403da29831908f54f4178ecf42e
Nishad Uzzaman <nishad@gmail.com>
2021-11-01 21:30:47 +0300

    Question Answers

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -8,10 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Answer extends Model
 {
     use HasFactory;
+    protected $with = ['user'];
 
     // question belongs to one question
     public function question()
     {
         return $this->belongsTo(Question::class, 'question_id');
+    }
+    // each question belongs to a given user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
